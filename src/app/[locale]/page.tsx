8480399876bc9382def9cd33014fec100e970ef4
unknown <mohamedadeladlymohamed@gmail.com>
2024-09-08 16:39:56 +0300

    remove lazy load lib

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,7 +3,6 @@ import { Metadata } from "next";
 import FeaturedStoresSection from "@/components/home/featuredStores.tsx/FeaturedStores";
 import { getTranslations } from "next-intl/server";
 import { Suspense } from "react";
-import LazyLoad from "react-lazy-load";
 import SwiperSection from "@/components/home/mainSlider/SwiperSection";
 import SwiperSkeleton from "@/components/home/mainSlider/SwiperSkeleton";
 import CouponInstruction from "@/components/home/couponsInstruction/CouponInstruction";
@@ -40,9 +39,7 @@ export default async function HomePage() {
         <FeaturedStoresSection />
       </section>
       <section className="my-12 text-start">
-        <LazyLoad>
           <CouponInstruction />
-        </LazyLoad>
       </section>
     </>
   );
