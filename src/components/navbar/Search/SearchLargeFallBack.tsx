bce4bb8f39e3b93607dd0014a1cf85c4f9bcf688
kyanlabs43 <kyanlabs43@gmail.com>
2024-09-16 13:46:32 +0300

    resolve searcbutton transaltion problem

--- src/components/navbar/Search/SearchLargeFallBack.tsx
+++ src/components/navbar/Search/SearchLargeFallBack.tsx
@@ -7,7 +7,7 @@ export default function SearchLargeFallBack() {
   const t = useTranslations("searchBar");
 
   return (
-    <form className="relative" onSubmit={(e) => e.preventDefault()}>
+    <form className="relative" >
       <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
         <Search className="h-5 w-5 text-muted-foreground cursor-pointer" />
       </div>
